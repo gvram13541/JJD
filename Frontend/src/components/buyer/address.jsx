c7f277e652919c2fe01d32d7dc3ca70c1847b3ef
Gunavardhan Reddy Perugu <guna955reddy@gmail.com>
2025-12-01 22:49:11 +0530

    fronted to display the saved addresses

--- Frontend/src/components/buyer/address.jsx
+++ Frontend/src/components/buyer/address.jsx
@@ -1,7 +1,43 @@
+import "../../styles/address.css";
+
 function MyAddresses() {
-    return(
-        <h2>Your saved addresses...</h2>
-    )
+    return (
+        <div className="address-container">
+            <h2 className="address-title">My Addresses</h2>
+
+            <table className="address-table">
+                <thead>
+                    <tr>
+                        <th>Village</th>
+                        <th>Street</th>
+                        <th>District</th>
+                        <th>State</th>
+                        <th>Pincode</th>
+                        <th>Default</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>2</td>
+                        <td>2</td>
+                        <td>2</td>
+                        <td>2</td>
+                        <td>No</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default MyAddresses;
